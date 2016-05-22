5de893ea6caa0a3069df7b59b3fc3c30c43473b8
Trevor Bača <trevor.baca@gmail.com>
2016-05-22 19:59:00 -0400

    Added empty stages.

--- dornen/__metadata__.py
+++ dornen/__metadata__.py
@@ -4,7 +4,12 @@ from abjad import *
 
 metadata = datastructuretools.TypedOrderedDict(
     [
-        ('title', 'Der Dorn'),
-        ('year', 2015),
+        ('catalog_number', 'AWN-019'),
+        ('composer', 'Ba\xc4\x8da'),
+        ('forces_tagline', 'for guitar'),
+        ('paper_dimensions', '17 x 11 in'),
+        ('price', '\\$80 / \\euro 72'),
+        ('title', 'Spiel der Dornen'),
+        ('year', 2016),
         ]
     )

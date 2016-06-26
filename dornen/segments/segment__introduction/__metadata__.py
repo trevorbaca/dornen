11857a88bc08b4d2b61118f46bd016199273493c
Trevor Bača <trevor.baca@gmail.com>
2016-06-26 15:56:02 -0500

    Introduction.

--- dornen/segments/segment__introduction/__metadata__.py
+++ dornen/segments/segment__introduction/__metadata__.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
-from abjad import *
+import abjad
 
 
-metadata = datastructuretools.TypedOrderedDict(
+metadata = abjad.datastructuretools.TypedOrderedDict(
     [
         (
             'end_clefs_by_staff',
-            datastructuretools.TypedOrderedDict(
+            abjad.datastructuretools.TypedOrderedDict(
                 [
                     ('Guitar Music Staff', 'treble'),
                     ]
@@ -14,7 +14,7 @@ metadata = datastructuretools.TypedOrderedDict(
             ),
         (
             'end_instruments_by_context',
-            datastructuretools.TypedOrderedDict(
+            abjad.datastructuretools.TypedOrderedDict(
                 [
                     ('Guitar Music Staff', 'guitar'),
                     ]

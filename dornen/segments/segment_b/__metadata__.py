b767c129c8ba98474ee20082b0d0c274f727d20a
Trevor Bača <trevor.baca@gmail.com>
2016-06-28 11:03:38 -0500

    [B].

--- dornen/segments/segment_b/__metadata__.py
+++ dornen/segments/segment_b/__metadata__.py
@@ -4,7 +4,26 @@ import abjad
 
 metadata = abjad.datastructuretools.TypedOrderedDict(
     [
+        (
+            'end_clefs_by_staff',
+            abjad.datastructuretools.TypedOrderedDict(
+                [
+                    ('Guitar Music Staff', 'treble'),
+                    ]
+                ),
+            ),
+        (
+            'end_instruments_by_context',
+            abjad.datastructuretools.TypedOrderedDict(
+                [
+                    ('Guitar Music Staff', 'guitar'),
+                    ]
+                ),
+            ),
+        ('end_tempo', None),
+        ('end_time_signature', '1/64'),
         ('first_bar_number', 34),
+        ('measure_count', 22),
         ('name', 'B'),
         ('segment_count', 15),
         ('segment_number', 3),
--- dornen/segments/segment_c/__metadata__.py
+++ dornen/segments/segment_c/__metadata__.py
@@ -4,6 +4,7 @@ import abjad
 
 metadata = abjad.datastructuretools.TypedOrderedDict(
     [
+        ('first_bar_number', 48),
         ('name', 'C'),
         ('segment_count', 15),
         ('segment_number', 4),

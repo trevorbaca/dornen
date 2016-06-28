8819baeec42487a6d2a8daaa494eb89447bb105e
Trevor Bača <trevor.baca@gmail.com>
2016-06-28 13:25:27 -0500

    Introduction.

--- dornen/segments/segment_i/__metadata__.py
+++ dornen/segments/segment_i/__metadata__.py
@@ -4,6 +4,7 @@ import abjad
 
 metadata = abjad.datastructuretools.TypedOrderedDict(
     [
+        ('first_bar_number', 154),
         ('name', 'I'),
         ('segment_count', 15),
         ('segment_number', 10),

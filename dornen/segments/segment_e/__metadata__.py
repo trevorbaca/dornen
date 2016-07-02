31cfeb2f8060fc78c15de120aa94650dbcf88393
Trevor Bača <trevor.baca@gmail.com>
2016-07-02 14:01:45 -0500

    [B].

--- dornen/segments/segment_e/__metadata__.py
+++ dornen/segments/segment_e/__metadata__.py
@@ -22,7 +22,7 @@ metadata = abjad.datastructuretools.TypedOrderedDict(
             ),
         ('end_tempo', None),
         ('end_time_signature', '5/32'),
-        ('first_bar_number', 83),
+        ('first_bar_number', 84),
         ('measure_count', 20),
         ('name', 'E'),
         ('segment_count', 15),

f4655a449d7e3af13d9d2e1e463e3bd6e9a277c7
Trevor Bača <trevor.baca@gmail.com>
2016-07-04 17:26:53 -0500

    [B].

---
+++ dornen/tools/DesignChecker.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+import abjad
+
+
+class DesignChecker(abjad.abctools.AbjadObject):
+    r'''Design-checker.
+    '''
+
+    ### CLASS VARIABLES ###
+
+    __slots__ = (
+        )
+
+    ### INITIALIZER ###
+
+    def __init__(self):
+        pass
+
+    ### SPECIAL METHODS ###
+
+    def __call__(self, design, score):
+        pass

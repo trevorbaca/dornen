7a1111116d1794ec2e98180ce37cbab6efa07592
Trevor Bača <trevor.baca@gmail.com>
2016-07-04 17:15:42 -0500

    [B].

---
+++ dornen/tools/make_rest_figure_maker.py
@@ -0,0 +1,299 @@
+# -*- coding: utf-8 -*-
+import abjad
+import baca
+
+
+def make_rest_figure_maker(duration=None):
+    r'''Makes rest figure maker.
+
+    ::
+
+        >>> import baca
+        >>> import dornen
+
+    ::
+
+        >>> print(format(dornen.tools.make_rest_figure_maker()))
+        baca.tools.FigureMaker(
+            baca.tools.RhythmSpecifier(
+                patterns=[
+                    patterntools.Pattern(
+                        indices=(0,),
+                        period=1,
+                        ),
+                    ],
+                rhythm_maker=baca.tools.FigureRhythmMaker(
+                    talea=rhythmmakertools.Talea(
+                        counts=(1,),
+                        denominator=8,
+                        ),
+                    ),
+                ),
+            annotate_unregistered_pitches=True,
+            preferred_denominator=8,
+            )
+
+    ..  container:: example
+
+        **Example 1.** Makes one-stage  rests:
+
+        ::
+
+            >>> stage_tokens = [
+            ...     [None],
+            ...     [None, None],
+            ...     [None, None, None],
+            ...     ]
+            >>> stage_tokens = datastructuretools.CyclicTuple(stage_tokens)
+            >>> figure_tokens = [
+            ...     stage_tokens[:1],
+            ...     stage_tokens[1:2],
+            ...     stage_tokens[2:3],
+            ...     ]
+            >>> for figure_token in figure_tokens:
+            ...     figure_token
+            ([None],)
+            ([None, None],)
+            ([None, None, None],)
+
+        ::
+
+            >>> figure_maker = dornen.tools.make_rest_figure_maker()
+            >>> figures, time_signatures = [], []
+            >>> for figure_token in figure_tokens:
+            ...     result = figure_maker(figure_token)
+            ...     selection, time_signature, state_manifest = result
+            ...     figures.append(selection)
+            ...     time_signatures.append(time_signature)    
+            ...
+            >>> figures_ = []
+            >>> for figure in figures:
+            ...     figures_.extend(figure)
+            ...
+            >>> figures = select(figures_)
+
+        ::
+
+            >>> segment_maker = baca.tools.SegmentMaker(
+            ...     ignore_unregistered_pitches=True,
+            ...     score_template=baca.tools.ViolinSoloScoreTemplate(),
+            ...     time_signatures=time_signatures,
+            ...     )
+            >>> specifiers = segment_maker.append_specifiers(
+            ...     ('vn', baca.tools.stages(1)),
+            ...     baca.tools.RhythmSpecifier(
+            ...         rhythm_maker=figures,
+            ...         ),
+            ...     )
+
+        ::
+
+            >>> result = segment_maker(is_doc_example=True)
+            >>> lilypond_file, segment_metadata = result
+            >>> score = lilypond_file.score_block.items[0]
+            >>> show(lilypond_file) # doctest: +SKIP
+
+        ..  doctest::
+
+            >>> score = lilypond_file.score_block.items[0]
+            >>> f(score)
+            \context Score = "Score" <<
+                \tag violin
+                \context TimeSignatureContext = "Time Signature Context" <<
+                    \context TimeSignatureContextMultimeasureRests = "Time Signature Context Multimeasure Rests" {
+                        {
+                            \time 1/8
+                            R1 * 1/8
+                        }
+                        {
+                            \time 2/8
+                            R1 * 1/4
+                        }
+                        {
+                            \time 3/8
+                            R1 * 3/8
+                        }
+                    }
+                    \context TimeSignatureContextSkips = "Time Signature Context Skips" {
+                        {
+                            \time 1/8
+                            s1 * 1/8
+                        }
+                        {
+                            \time 2/8
+                            s1 * 1/4
+                        }
+                        {
+                            \time 3/8
+                            s1 * 3/8
+                        }
+                    }
+                >>
+                \context MusicContext = "Music Context" <<
+                    \tag violin
+                    \context ViolinMusicStaff = "Violin Music Staff" {
+                        \clef "treble"
+                        \context ViolinMusicVoice = "Violin Music Voice" {
+                            {
+                                {
+                                    r8
+                                }
+                            }
+                            {
+                                {
+                                    r8
+                                    r8
+                                }
+                            }
+                            {
+                                {
+                                    r8
+                                    r8
+                                    r8
+                                    \bar "|"
+                                }
+                            }
+                        }
+                    }
+                >>
+            >>
+
+    ..  container:: example
+
+        **Example 2.** Makes multistage rests:
+
+        ::
+
+            >>> stage_tokens = [
+            ...     [None],
+            ...     [None, None],
+            ...     [None, None, None],
+            ...     ]
+            >>> stage_tokens = datastructuretools.CyclicTuple(stage_tokens)
+            >>> figure_tokens = [
+            ...     stage_tokens[:3],
+            ...     stage_tokens[1:4],
+            ...     ]
+            >>> for figure_token in figure_tokens:
+            ...     figure_token
+            ([None], [None, None], [None, None, None])
+            ([None, None], [None, None, None], [None])
+
+        ::
+
+            >>> figure_maker = dornen.tools.make_rest_figure_maker()
+            >>> figures, time_signatures = [], []
+            >>> for figure_token in figure_tokens:
+            ...     result = figure_maker(figure_token)
+            ...     selection, time_signature, state_manifest = result
+            ...     figures.append(selection)
+            ...     time_signatures.append(time_signature)    
+            ...
+            >>> figures_ = []
+            >>> for figure in figures:
+            ...     figures_.extend(figure)
+            ...
+            >>> figures = select(figures_)
+
+        ::
+
+            >>> segment_maker = baca.tools.SegmentMaker(
+            ...     ignore_unregistered_pitches=True,
+            ...     score_template=baca.tools.ViolinSoloScoreTemplate(),
+            ...     time_signatures=time_signatures,
+            ...     )
+            >>> specifiers = segment_maker.append_specifiers(
+            ...     ('vn', baca.tools.stages(1)),
+            ...     baca.tools.RhythmSpecifier(
+            ...         rhythm_maker=figures,
+            ...         ),
+            ...     )
+
+        ::
+
+            >>> result = segment_maker(is_doc_example=True)
+            >>> lilypond_file, segment_metadata = result
+            >>> score = lilypond_file.score_block.items[0]
+            >>> show(lilypond_file) # doctest: +SKIP
+
+        ..  doctest::
+
+            >>> score = lilypond_file.score_block.items[0]
+            >>> f(score)
+            \context Score = "Score" <<
+                \tag violin
+                \context TimeSignatureContext = "Time Signature Context" <<
+                    \context TimeSignatureContextMultimeasureRests = "Time Signature Context Multimeasure Rests" {
+                        {
+                            \time 6/8
+                            R1 * 3/4
+                        }
+                        {
+                            R1 * 3/4
+                        }
+                    }
+                    \context TimeSignatureContextSkips = "Time Signature Context Skips" {
+                        {
+                            s1 * 3/4
+                        }
+                        {
+                            s1 * 3/4
+                        }
+                    }
+                >>
+                \context MusicContext = "Music Context" <<
+                    \tag violin
+                    \context ViolinMusicStaff = "Violin Music Staff" {
+                        \clef "treble"
+                        \context ViolinMusicVoice = "Violin Music Voice" {
+                            {
+                                {
+                                    r8
+                                }
+                                {
+                                    r8
+                                    r8
+                                }
+                                {
+                                    r8
+                                    r8
+                                    r8
+                                }
+                            }
+                            {
+                                {
+                                    r8
+                                    r8
+                                }
+                                {
+                                    r8
+                                    r8
+                                    r8
+                                }
+                                {
+                                    r8
+                                    \bar "|"
+                                }
+                            }
+                        }
+                    }
+                >>
+            >>
+
+    Returns figure maker.
+    '''
+    duration = duration or abjad.Duration(1, 8)
+    figure_maker = baca.tools.FigureMaker(
+        baca.tools.RhythmSpecifier(
+            patterns=abjad.patterntools.select_all(),
+            rhythm_maker=baca.tools.FigureRhythmMaker(
+                talea=abjad.rhythmmakertools.Talea(
+                    counts=[duration.numerator],
+                    denominator=duration.denominator,
+                    ),
+                )
+            ),
+        annotate_unregistered_pitches=True,
+        preferred_denominator=duration.denominator,
+        )
+    return figure_maker

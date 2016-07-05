1bfe729c93180e1d893ad91a52e489ead4117b2c
Trevor Bača <trevor.baca@gmail.com>
2016-07-05 11:57:28 -0500

    [B].

--- dornen/segments/segment_b/definition.py
+++ dornen/segments/segment_b/definition.py
@@ -219,9 +219,9 @@ spacing_specifier = baca.tools.SpacingSpecifier(
 measures_per_stage = len(accumulator.time_signatures) * [1]
 
 segment_maker = baca.tools.SegmentMaker(
+    allow_figure_names=True,
     #design_checker=dornen.tools.DesignChecker(design=design),
     #label_clock_time=True,
-    #label_figure_names=True,
     #label_stages=True,
     measures_per_stage=measures_per_stage,
     score_package=dornen,

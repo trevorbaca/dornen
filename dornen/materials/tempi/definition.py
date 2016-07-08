bf5e1c6517d4ac5911a9085adb50a93830d7e252
Trevor Bača <trevor.baca@gmail.com>
2016-07-08 18:51:57 -0500

    [F].

--- dornen/materials/tempi/definition.py
+++ dornen/materials/tempi/definition.py
@@ -1,24 +1,23 @@
 # -*- coding: utf-8 -*-
+import abjad
 import collections
-from abjad.tools import durationtools
-from abjad.tools import indicatortools
 
 
 tempi = collections.OrderedDict([
     (
         44,
-        indicatortools.Tempo(durationtools.Duration(1, 8), 44),
+        abjad.Tempo(abjad.Duration(1, 8), 44),
         ),
     (
         66,
-        indicatortools.Tempo(durationtools.Duration(1, 8), 66),
+        abjad.Tempo(abjad.Duration(1, 8), 66),
         ),
     (
         90,
-        indicatortools.Tempo(durationtools.Duration(1, 8), 90),
+        abjad.Tempo(abjad.Duration(1, 8), 90),
         ),
     (
         100,
-        indicatortools.Tempo(durationtools.Duration(1, 8), 100),
+        abjad.Tempo(abjad.Duration(1, 8), 100),
         ),
     ])

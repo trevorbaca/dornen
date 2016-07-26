821cc891e03334e5778da093e63353daa60df604
Trevor Bača <trevor.baca@gmail.com>
2016-07-26 12:08:25 -0500

    Rebuilt score.

--- dornen/__metadata__.py
+++ dornen/__metadata__.py
@@ -7,8 +7,6 @@ metadata = abjad.datastructuretools.TypedOrderedDict(
         ('catalog_number', 'AWN-019'),
         ('composer', 'Ba\xc4\x8da'),
         ('forces_tagline', 'for guitar'),
-        ('paper_size', '17 x 11 in'),
-        ('price', '\\$80 / \\euro 72'),
         ('title', 'Spiel der Dornen'),
         ('year', 2016),
         ]

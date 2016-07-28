5e20220e6a2029df0ed979a51343163d3b00c560
Trevor Bača <trevor.baca@gmail.com>
2016-07-28 10:38:26 -0500

    Rebuilt score.

--- dornen/__metadata__.py
+++ dornen/__metadata__.py
@@ -4,7 +4,7 @@ import abjad
 
 metadata = abjad.datastructuretools.TypedOrderedDict(
     [
-        ('catalog_number', 'AWN-019'),
+        ('catalog_number', 'AWN-016'),
         ('composer', 'Ba\xc4\x8da'),
         ('forces_tagline', 'for guitar'),
         ('title', 'Spiel der Dornen'),
--- dornen/build/a3-landscape/__metadata__.py
+++ dornen/build/a3-landscape/__metadata__.py
@@ -1,1 +1,11 @@
 # -*- coding: utf-8 -*-
+import abjad
+
+
+metadata = abjad.datastructuretools.TypedOrderedDict(
+    [
+        ('catalog_number_suffix', 'A3.L'),
+        ('paper_size', 'a3'),
+        ('price', '\\$52 / \\euro 46'),
+        ]
+    )

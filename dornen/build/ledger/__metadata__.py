1c1c1af277cb763f975fe05226acec4adb182f89
Trevor Bača <trevor.baca@gmail.com>
2016-08-05 08:22:10 -0500

    Rebuilt score.

---
+++ dornen/build/ledger/__metadata__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+import abjad
+
+
+metadata = abjad.datastructuretools.TypedOrderedDict(
+    [
+        ('catalog_number_suffix', ''),
+        ('paper_size', 'ledger'),
+        ('price', '\\$52 / \\euro 46'),
+        ]
+    )

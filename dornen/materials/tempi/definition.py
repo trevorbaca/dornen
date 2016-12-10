b0afa5c50b011c8400d8c34427d502e101609e98
Trevor Bača <trevor.baca@gmail.com>
2016-12-10 13:33:55 -0600

    Updated import statements.

--- dornen/materials/tempi/definition.py
+++ dornen/materials/tempi/definition.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
 import abjad
-import collections
 
 
-tempi = collections.OrderedDict([
+tempi = abjad.datastructuretools.TypedOrderedDict([
     (
         44,
         abjad.Tempo(abjad.Duration(1, 8), 44),

65b6a5b7cb4d023eb22f3c0aa500d21f39c24471
Trevor Bača <trevor.baca@gmail.com>
2017-04-29 19:52:06 -0500

    Cleaned up package initializer.

--- dornen/__init__.py
+++ dornen/__init__.py
@@ -1,4 +1,13 @@
 # -*- coding: utf-8 -*-
+import distutils.version
+import platform
+if not (
+    distutils.version.LooseVersion('3.5') <
+    distutils.version.LooseVersion(platform.python_version())
+    ):
+    raise ImportError('Requires Python 3.5.')
+del distutils
+del platform
 from dornen import tools
 from dornen import materials
 from dornen import segments

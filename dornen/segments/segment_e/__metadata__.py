bf110eecb7b8624f0e056fde77934b187f8dfec6
Trevor Bača <trevor.baca@gmail.com>
2017-08-07 09:11:08 -0700

    Rerendered segments.

--- dornen/segments/segment_e/__metadata__.py
+++ dornen/segments/segment_e/__metadata__.py
@@ -20,7 +20,7 @@ metadata = abjad.TypedOrderedDict(
                     ]
                 ),
             ),
-        ('end_tempo', None),
+        ('end_tempo', 44),
         ('end_time_signature', '125/64'),
         ('first_bar_number', 120),
         ('measure_count', 13),

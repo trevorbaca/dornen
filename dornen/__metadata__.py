297ff8b36addd696346e211afcfbdfa212f4a52d
Trevor Bača <trevor.baca@gmail.com>
2017-08-16 11:31:55 -0500

    Cleaned up metadat.

--- dornen/__metadata__.py
+++ dornen/__metadata__.py
@@ -4,7 +4,7 @@ import abjad
 metadata = abjad.TypedOrderedDict(
     [
         ('catalog_number', 'AWN-016'),
-        ('composer', 'Bača'),
+        ('composer', 'Trevor Bača'),
         ('forces_tagline', 'for guitar'),
         ('title', 'Spiel der Dornen'),
         ('year', 2016),

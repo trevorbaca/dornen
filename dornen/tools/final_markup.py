303e3e2d08ad6998b5a3cdf99c453986cd4b0b61
Trevor Bača <trevor.baca@gmail.com>
2017-08-18 15:45:28 -0500

    Cleaned up final markup.

--- dornen/tools/final_markup.py
+++ dornen/tools/final_markup.py
@@ -4,12 +4,72 @@ import abjad
 def final_markup():
     r'''Makes final markup.
 
-    Returns markup.
+    ::
+
+        >>> import dornen
+
+    ..  container:: example
+
+        ::
+
+            >>> markup = dornen.final_markup()
+            >>> show(markup) # doctest: +SKIP
+
+        ..  docs::
+
+            >>> f(markup)
+            _ \markup {
+                \with-color
+                    #black
+                    \italic
+                        \right-column
+                            {
+                                \line
+                                    {
+                                        Cambridge,
+                                        MA
+                                        \hspace
+                                            #0.75
+                                        –
+                                        \hspace
+                                            #0.75
+                                        Madison,
+                                        WI
+                                        \hspace
+                                            #0.75
+                                        –
+                                        \hspace
+                                            #0.75
+                                        Palo
+                                        Alto,
+                                        CA.
+                                    }
+                                \line
+                                    {
+                                        November
+                                        2015
+                                        \hspace
+                                            #0.75
+                                        –
+                                        \hspace
+                                            #0.75
+                                        July
+                                        2016.
+                                    }
+                            }
+                }
+
     '''
-    place_markup = abjad.Markup('Cambridge, MA – Madison, WI – Palo Alto, CA.')
-    date_markup = abjad.Markup('Nov. 2015 – Jul. 2016.')
-    final_markup = abjad.Markup.right_column([place_markup, date_markup])
-    final_markup = final_markup.italic()
-    final_markup = final_markup.with_color('black')
-    final_markup = abjad.new(final_markup, direction=Down)
-    return final_markup
+    place = ['Cambridge, MA', 'Madison, WI', 'Palo Alto, CA.']
+    place = r' \hspace #0.75 – \hspace #0.75 '.join(place)
+    place = abjad.Markup(place)
+    place = abjad.Markup.line([place])
+    date = ['November 2015', 'July 2016.']
+    date = r' \hspace #0.75 – \hspace #0.75 '.join(date)
+    date = abjad.Markup(date)
+    date = abjad.Markup.line([date])
+    markup = abjad.Markup.right_column([place, date])
+    markup = markup.italic()
+    markup = markup.with_color('black')
+    markup = abjad.new(markup, direction=abjad.Down)
+    return markup

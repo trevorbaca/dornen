def9e45c7660791db8cf1a6861d7c7ae67286862
Trevor Bača <trevor.baca@gmail.com>
2017-09-22 14:40:27 -0500

    Cleaned up context definitions.

--- dornen/tools/ScoreTemplate.py
+++ dornen/tools/ScoreTemplate.py
@@ -17,7 +17,7 @@ class ScoreTemplate(baca.ScoreTemplate):
 
             >>> template = dornen.ScoreTemplate()
             >>> path = pathlib.Path(dornen.__path__[0])
-            >>> path = path / 'stylesheets' / 'context-definitions.ily'
+            >>> path = path / 'stylesheets' / 'contexts.ily'
             >>> lilypond_file = template.__illustrate__(
             ...     global_staff_size=16,
             ...     includes=[path],

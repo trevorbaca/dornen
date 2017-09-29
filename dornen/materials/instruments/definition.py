967a1a6323f133ff80f5d3319a334194809a6981
Trevor Bača <trevor.baca@gmail.com>
2017-09-29 10:05:17 -0500

    Cleaned up instruments.

--- dornen/materials/instruments/definition.py
+++ dornen/materials/instruments/definition.py
@@ -5,7 +5,7 @@ import baca
 instruments = abjad.TypedOrderedDict([
     (
         'guitar',
-        abjad.instrumenttools.Guitar(
+        abjad.Guitar(
             name_markup=baca.markup.instrument('Guitar'),
             short_name_markup=abjad.Markup.null(),
             ),

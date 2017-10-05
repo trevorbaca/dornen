d31346f194d78d43eca89d1b8f91e93e7a117be2
Trevor Bača <trevor.baca@gmail.com>
2017-10-05 10:47:15 -0500

    Cleaned up score template.

--- dornen/tools/ScoreTemplate.py
+++ dornen/tools/ScoreTemplate.py
@@ -61,15 +61,6 @@ class ScoreTemplate(baca.ScoreTemplate):
 
     '''
 
-    ### CLASS VARIABLES ###
-
-    voice_abbreviations = {
-        'v1': 'Guitar Music Voice 1',
-        'v2': 'Guitar Music Voice 2',
-        'v3': 'Guitar Music Voice 3',
-        'v4': 'Guitar Music Voice 4',
-        }
-
     ### SPECIAL METHODS ###
 
     def __call__(self):

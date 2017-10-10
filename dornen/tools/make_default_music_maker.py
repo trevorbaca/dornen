f7d1689413dceedb0c42fb738611d647bbdc880f
Trevor Bača <trevor.baca@gmail.com>
2017-10-10 10:30:31 -0500

    Cleaned up defintiions.

--- dornen/tools/make_default_music_maker.py
+++ dornen/tools/make_default_music_maker.py
@@ -225,9 +225,8 @@ def make_default_music_maker():
             ...         ),
             ...     time_signatures=time_signatures,
             ...     )
-            >>> segment_maker.append_commands(
-            ...     'Violin Music Voice',
-            ...     baca.select_stages(1),
+            >>> segment_maker.scope(
+            ...     baca.scope('Violin Music Voice', 1),
             ...     baca.tools.RhythmCommand(
             ...         rhythm_maker=figures,
             ...         ),

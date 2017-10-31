db3e0fd17edfd944225962f1f40d2e3c2546f63a
Trevor Bača <trevor.baca@gmail.com>
2017-10-31 09:36:17 -0500

    Integrating tie changes.

--- dornen/segments/D/definition.py
+++ dornen/segments/D/definition.py
@@ -214,7 +214,7 @@ accumulator(
     [['Gb2']],
     accumulator.monad_music_maker,
     baca.markup.boxed_specifier('2-finger tamb. trill'),
-    baca.messiaen_tie(),
+    baca.repeat_tie(),
     baca.stem_tremolo(),
     baca.stems_up(),
     baca.tuplet_bracket_staff_padding(0),

b1b69a9a6deaa921f398c0ff70a963dc0c31035f
Trevor Bača <trevor.baca@gmail.com>
2017-11-08 10:57:56 -0600

    Integrating API changes.

--- dornen/segments/C/definition.py
+++ dornen/segments/C/definition.py
@@ -202,7 +202,7 @@ spacing_specifier = baca.HorizontalSpacingSpecifier(
 measures_per_stage = len(accumulator.time_signatures) * [1]
 
 segment_maker = baca.SegmentMaker(
-    #allow_figure_names=True,
+    allow_figure_names=False,
     ignore_repeat_pitch_classes=True,
     instruments=dornen.instruments,
     label_clock_time=False,

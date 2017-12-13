91ce5b14959b892f3d8171801cede89c021253db
Trevor Bača <trevor.baca@gmail.com>
2017-12-13 11:38:40 -0600

    Rerendered materials.

--- dornen/materials/metronome_marks/definition.py
+++ dornen/materials/metronome_marks/definition.py
@@ -4,10 +4,10 @@ import abjad
 metronome_marks = abjad.MetronomeMarkDictionary([
     (
         '44',
-        abjad.MetronomeMark(abjad.Duration(1, 8), 44),
+        abjad.MetronomeMark((1, 8), 44),
         ),
     (
         '66',
-        abjad.MetronomeMark(abjad.Duration(1, 8), 66),
+        abjad.MetronomeMark((1, 8), 66),
         ),
     ])

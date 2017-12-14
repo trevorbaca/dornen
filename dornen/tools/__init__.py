0f4ecd7e5722a9e50b762e18fb0d655e3ceda660
Trevor Bača <trevor.baca@gmail.com>
2017-12-14 16:14:15 -0600

    Cleaned up initializers.

--- dornen/tools/__init__.py
+++ dornen/tools/__init__.py
@@ -1,7 +1,28 @@
-import abjad
-
-
-abjad.ImportManager.import_structured_package(
-    __path__[0],
-    globals(),
-    )
+from .DesignChecker import DesignChecker
+from .MusicAccumulator import MusicAccumulator
+from .QuartertoneCommand import QuartertoneCommand
+from .ScoreTemplate import ScoreTemplate
+from .anchor_music import anchor_music
+from .default_music import default_music
+from .delicatissimo_music import delicatissimo_music
+from .design_1 import design_1
+from .design_2 import design_2
+from .design_3 import design_3
+from .forty_eighth_music import forty_eighth_music
+from .glissando_scatto_music import glissando_scatto_music
+from .graced_rest_music import graced_rest_music
+from .graced_tuplet_music import graced_tuplet_music
+from .monad_music import monad_music
+from .ovoid_music import ovoid_music
+from .passepied_music import passepied_music
+from .rest_music import rest_music
+from .running_music import running_music
+from .sixteenth_music import sixteenth_music
+from .sixty_fourth_music import sixty_fourth_music
+from .third_music import third_music
+from .thirty_second_music import thirty_second_music
+from .tremolo_music import tremolo_music
+from .twentieth_music import twentieth_music
+from .twenty_eighth_music import twenty_eighth_music
+from .twenty_fourth_music import twenty_fourth_music
+from .wave_music import wave_music

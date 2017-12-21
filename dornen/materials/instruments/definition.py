f8050fedb2365a129f24c066ab33b28d27a63559
Trevor Bača <trevor.baca@gmail.com>
2017-12-21 10:22:00 -0600

    Cleaning up instrument tags.

--- dornen/materials/instruments/definition.py
+++ dornen/materials/instruments/definition.py
@@ -6,8 +6,8 @@ instruments = abjad.InstrumentDictionary([
     (
         'Guitar',
         abjad.Guitar(
-            name_markup=baca.markup.instrument('Guitar'),
-            short_name_markup=abjad.Markup.null(),
+            markup=baca.markup.instrument('Guitar'),
+            short_markup=abjad.Markup.null(),
             ),
         ),
     ])

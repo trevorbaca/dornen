2b5347134483e33999232551d447d05babb26e9c
Trevor Bača <trevor.baca@gmail.com>
2018-06-26 12:25:08 -0500

    Cleaned up instrument definitions.

--- dornen/materials/instruments/definition.py
+++ dornen/materials/instruments/definition.py
@@ -5,6 +5,6 @@ import baca
 instruments = abjad.OrderedDict([
     (
         'Guitar',
-        abjad.Guitar(hide=True, pitch_range='[E2, F5]'),
+        abjad.Guitar(pitch_range='[E2, F5]'),
         ),
     ])

66a037dfa990dfb92c0c6b02a52d57b3296d1123
Trevor Bača <trevor.baca@gmail.com>
2019-02-26 09:38:26 -0600

    Cleaned up persist files.

--- dornen/segments/C/__persist__.py
+++ dornen/segments/C/__persist__.py
@@ -1,4 +1,44 @@
 import abjad
 
 
-persist = abjad.OrderedDict()
+persist = abjad.OrderedDict(
+    [
+        (
+            'persistent_indicators',
+            abjad.OrderedDict(
+                [
+                    (
+                        'Guitar_Music_Staff',
+                        [
+                            abjad.Momento(
+                                context='Guitar_Music_Voice_I',
+                                manifest='instruments',
+                                value='Guitar',
+                                ),
+                            abjad.Momento(
+                                context='Guitar_Music_Voice_I',
+                                prototype='abjad.Clef',
+                                value='treble',
+                                ),
+                            ],
+                        ),
+                    (
+                        'Score',
+                        [
+                            abjad.Momento(
+                                context='Global_Skips',
+                                manifest='metronome_marks',
+                                value='66',
+                                ),
+                            abjad.Momento(
+                                context='Global_Skips',
+                                prototype='abjad.TimeSignature',
+                                value='9/64',
+                                ),
+                            ],
+                        ),
+                    ]
+                ),
+            ),
+        ]
+    )

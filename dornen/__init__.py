5e4c1d6232e76883cbbbd47dd557527a6c93e9ef
Trevor Bača <trevor.baca@gmail.com>
2019-03-02 16:20:16 -0600

    Cleaned up typehints.

--- dornen/__init__.py
+++ dornen/__init__.py
@@ -8,7 +8,14 @@ if not (
 del distutils
 del platform
 from dornen.tools import *
-from dornen.materials import *
+from dornen.materials.blue_pitch_classes.definition import blue_pitch_classes
+from dornen.materials.bright_green_pitch_classes.definition import \
+    bright_green_pitch_classes
+from dornen.materials.green_pitch_classes.definition import green_pitch_classes
+from dornen.materials.instruments.definition import instruments
+from dornen.materials.magenta_pitch_classes.definition import \
+    magenta_pitch_classes
+from dornen.materials.metronome_marks.definition import metronome_marks
 from dornen import segments
 
 """

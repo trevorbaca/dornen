3e701585f77ad34576b13c7b58e9494329282824
Trevor Bača <trevor.baca@gmail.com>
2019-07-29 15:29:55 -0400

    Cleaned up acciaccatura specifiers.

--- dornen/tools/passepied_music.py
+++ dornen/tools/passepied_music.py
@@ -8,12 +8,7 @@ def passepied_music() -> list:
     Makes passepied commands.
     """
     return [
-        baca.pitch_first(
-            [1],
-            32,
-            acciaccatura=baca.AcciaccaturaSpecifier(),
-            pattern=abjad.index([0]),
-        ),
+        baca.pitch_first([1], 32, acciaccatura=baca.AcciaccaturaSpecifier()),
         baca.pitch_first([1], 16, pattern=~abjad.index([0])),
         rmakers.beam_groups(beam_lone_notes=True),
     ]

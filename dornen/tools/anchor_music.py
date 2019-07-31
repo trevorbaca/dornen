a6fd160c8840e4c34851779faf3b63c4a3b1d43e
Trevor Bača <trevor.baca@gmail.com>
2019-07-31 17:38:42 -0400

    Cleaned up rhythm construction.

--- dornen/tools/anchor_music.py
+++ dornen/tools/anchor_music.py
@@ -7,6 +7,8 @@ def anchor_music() -> list:
     Makes anchor commands.
     """
     return [
-        baca.pitch_first_assignment([3], 16, signature=8),
+        baca.pitch_first_command(
+            baca.pitch_first_assignment([3], 16, signature=8)
+        ),
         rmakers.beam(baca.select(), beam_lone_notes=True),
     ]

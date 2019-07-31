ee4f4e24fcda5d94664886d642c4a5998202f37b
Trevor Bača <trevor.baca@gmail.com>
2019-07-31 17:48:47 -0400

    Cleaned up rhythm construction.

--- dornen/tools/graced_tuplet_music.py
+++ dornen/tools/graced_tuplet_music.py
@@ -8,24 +8,26 @@ def graced_tuplet_music() -> list:
     Makes graced tuplet commands.
     """
     return [
-        baca.pitch_first_assignment(
-            [1],
-            16,
-            acciaccatura=baca.lmr(
-                left_length=1, right_counts=[2], right_cyclic=True
+        baca.pitch_first_command(
+            baca.pitch_first_assignment(
+                [1],
+                16,
+                acciaccatura=baca.lmr(
+                    left_length=1, right_counts=[2], right_cyclic=True
+                ),
+                pattern=abjad.index([0], 2),
+                signature=14,
+                treatments=["7:8"],
             ),
-            pattern=abjad.index([0], 2),
-            signature=14,
-            treatments=["7:8"],
-        ),
-        baca.pitch_first_assignment(
-            [1],
-            16,
-            acciaccatura=baca.lmr(
-                left_length=1, right_counts=[2], right_cyclic=True
+            baca.pitch_first_assignment(
+                [1],
+                16,
+                acciaccatura=baca.lmr(
+                    left_length=1, right_counts=[2], right_cyclic=True
+                ),
+                pattern=abjad.index([1], 2),
+                treatments=["7:5"],
             ),
-            pattern=abjad.index([1], 2),
-            treatments=["7:5"],
         ),
         rmakers.beam_groups(beam_lone_notes=True),
     ]

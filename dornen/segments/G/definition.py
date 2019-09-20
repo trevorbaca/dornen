c4022702d317b4c3bad4f571db511d4724ce657a
Trevor Bača <trevor.baca@gmail.com>
2019-09-20 16:32:22 -0400

    Rerendered segments.

--- dornen/segments/G/definition.py
+++ dornen/segments/G/definition.py
@@ -218,6 +218,7 @@ accumulator(
 )
 
 maker = baca.SegmentMaker(
+    check_all_are_pitched=True,
     segment_directory=abjad.Path(os.path.realpath(__file__)).parent,
     skips_instead_of_rests=True,
     time_signatures=accumulator.time_signatures,

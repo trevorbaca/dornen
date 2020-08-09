b67991885507ceec33bf77d3066afd710603d959
Trevor Bača <trevor.baca@gmail.com>
2020-08-09 13:08:14 -0400

    Cleaned up path management.

--- dornen/segments/E/definition.py
+++ dornen/segments/E/definition.py
@@ -256,7 +256,7 @@ accumulator(
 maker = baca.SegmentMaker(
     check_all_are_pitched=True,
     do_not_check_beamed_long_notes=True,
-    segment_directory=abjad.Path(__file__).resolve().parent,
+    segment_directory=baca.Path(__file__).resolve().parent,
     skips_instead_of_rests=True,
     time_signatures=accumulator.time_signatures,
     transpose_score=True,

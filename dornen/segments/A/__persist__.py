decb85942042abad20eca9b492f171572cabd71b
Trevor Bača <trevor.baca@gmail.com>
2020-08-27 13:07:50 -0400

    Blacked code.

--- dornen/segments/A/__persist__.py
+++ dornen/segments/A/__persist__.py
@@ -5,60 +5,60 @@ import ide
 persist = abjad.OrderedDict(
     [
         (
-            'alive_during_segment',
+            "alive_during_segment",
             [
-                'Score',
-                'Global_Context',
-                'Global_Skips',
-                'Music_Context',
-                'Guitar_Music_Staff',
-                'Global_Rests',
-                'Guitar_Music_Voice_I',
-                'Guitar_Rest_Voice_I',
-                'Guitar_Music_Voice_II',
-                'Guitar_Rest_Voice_II',
-                'Guitar_Music_Voice_III',
-                'Guitar_Rest_Voice_III',
-                'Guitar_Music_Voice_IV',
-                'Guitar_Rest_Voice_IV',
-                ],
-            ),
+                "Score",
+                "Global_Context",
+                "Global_Skips",
+                "Music_Context",
+                "Guitar_Music_Staff",
+                "Global_Rests",
+                "Guitar_Music_Voice_I",
+                "Guitar_Rest_Voice_I",
+                "Guitar_Music_Voice_II",
+                "Guitar_Rest_Voice_II",
+                "Guitar_Music_Voice_III",
+                "Guitar_Rest_Voice_III",
+                "Guitar_Music_Voice_IV",
+                "Guitar_Rest_Voice_IV",
+            ],
+        ),
         (
-            'persistent_indicators',
+            "persistent_indicators",
             abjad.OrderedDict(
                 [
                     (
-                        'Guitar_Music_Staff',
+                        "Guitar_Music_Staff",
                         [
                             ide.Momento(
-                                context='Guitar_Music_Voice_I',
-                                manifest='instruments',
-                                value='Guitar',
-                                ),
+                                context="Guitar_Music_Voice_I",
+                                manifest="instruments",
+                                value="Guitar",
+                            ),
                             ide.Momento(
-                                context='Guitar_Music_Voice_I',
-                                prototype='abjad.Clef',
-                                value='treble',
-                                ),
-                            ],
-                        ),
+                                context="Guitar_Music_Voice_I",
+                                prototype="abjad.Clef",
+                                value="treble",
+                            ),
+                        ],
+                    ),
                     (
-                        'Score',
+                        "Score",
                         [
                             ide.Momento(
-                                context='Global_Skips',
-                                manifest='metronome_marks',
-                                value='44',
-                                ),
+                                context="Global_Skips",
+                                manifest="metronome_marks",
+                                value="44",
+                            ),
                             ide.Momento(
-                                context='Global_Skips',
-                                prototype='abjad.TimeSignature',
-                                value='12/64',
-                                ),
-                            ],
-                        ),
-                    ]
-                ),
+                                context="Global_Skips",
+                                prototype="abjad.TimeSignature",
+                                value="12/64",
+                            ),
+                        ],
+                    ),
+                ]
             ),
-        ]
-    )
+        ),
+    ]
+)

041a616f549a57fca16a6e8edd4f963d1fa3e55a
Trevor Bača <trevor.baca@gmail.com>
2021-04-24 16:17:24 -0400

    Cleaned up selectors.

--- dornen/library.py
+++ dornen/library.py
@@ -261,7 +261,7 @@ def glissando_scatto() -> list:
     return [
         baca.figure([2, 2, 2, 1, 2, 2, 2], 32, signature=16, treatments=[-2]),
         rmakers.beam_groups(),
-        baca.glissando(map=baca.ntruns()),
+        baca.glissando(map=baca.selectors.ntruns()),
     ]
 
 
--- dornen/segments/13/definition.py
+++ dornen/segments/13/definition.py
@@ -355,7 +355,7 @@ maker(
     baca.register(-20),
     baca.new(
         baca.repeat_tie(baca.pleaves()[1:]),
-        map=baca.qruns(),
+        map=baca.selectors.qruns(),
     ),
     baca.stem_tremolo(baca.pleaves()),
 )

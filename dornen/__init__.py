2da27115d562491c7e065643b8ab94aa959b2b22
Trevor Bača <trevor.baca@gmail.com>
2021-07-28 15:49:38 -0400

    Taught package initializer to check for Python 3.9 or later.

--- dornen/__init__.py
+++ dornen/__init__.py
@@ -1,5 +1,4 @@
-import distutils.version
-import platform
+import sys
 
 from .library import (
     DesignChecker,
@@ -65,13 +64,9 @@ __all__ = [
     "DesignChecker",
 ]
 
-if not (
-    distutils.version.LooseVersion("3.7")
-    < distutils.version.LooseVersion(platform.python_version())
-):
-    raise ImportError("Requires Python 3.7.")
-del distutils
-del platform
+if sys.version_info[:2] < (3, 9):
+    raise ImportError("Requires Python 3.9 or later")
+del sys
 
 """
 [intro]: 40"

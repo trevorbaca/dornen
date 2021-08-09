46c57a3ca8773e3971b3baf4fd5aa5d342c9a70f
Trevor Bača <trevor.baca@gmail.com>
2021-08-09 16:41:56 -0400

    Cleaned up layout files.

--- dornen/segments/01/layout.py
+++ dornen/segments/01/layout.py
@@ -15,7 +15,6 @@ breaks = baca.breaks(
 )
 
 spacing = baca.spacing(
-    __file__,
     breaks=breaks,
     fallback_duration=(1, 72),
 )
--- dornen/segments/06/layout.py
+++ dornen/segments/06/layout.py
@@ -10,7 +10,6 @@ breaks = baca.breaks(
 )
 
 spacing = baca.spacing(
-    __file__,
     breaks=breaks,
     fallback_duration=(1, 180),
 )

7caa4d47578038eb8117de6db6762f39dd2a79dd
Trevor Bača <trevor.baca@gmail.com>
2021-08-24 15:03:27 -0400

    Cleaned up layout files.

--- dornen/segments/09/layout.py
+++ dornen/segments/09/layout.py
@@ -1,13 +1,11 @@
 import baca
 
-spacing = baca.SpacingSpecifier(
-    breaks=baca.breaks(
-        baca.page(
-            1,
-            baca.system(measure=1, y_offset=20, distances=(20,)),
-            baca.system(measure=8, y_offset=80, distances=(20,)),
-            baca.system(measure=16, y_offset=140, distances=(20,)),
-        ),
+spacing = baca.make_layout(
+    baca.page(
+        1,
+        baca.system(measure=1, y_offset=20, distances=(20,)),
+        baca.system(measure=8, y_offset=80, distances=(20,)),
+        baca.system(measure=16, y_offset=140, distances=(20,)),
     ),
     fallback_duration=(1, 180),
 )

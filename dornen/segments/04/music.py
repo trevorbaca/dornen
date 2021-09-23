ce8cde27e6218a0d948acb58c738293379319b21
Trevor Bača <trevor.baca@gmail.com>
2021-09-23 11:52:21 +0200

    Cleaned up definition files.

--- dornen/segments/04/music.py
+++ dornen/segments/04/music.py
@@ -196,6 +196,7 @@ accumulator(
 )
 
 maker = baca.SegmentMaker(
+    **baca.segments(),
     error_on_not_yet_pitched=True,
     do_not_check_beamed_long_notes=True,
     ignore_repeat_pitch_classes=True,

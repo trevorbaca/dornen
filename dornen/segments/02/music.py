8b192c422986149546e3fee9340acbca9d05bb18
Trevor Bača <trevor.baca@gmail.com>
2021-09-24 12:23:16 +0200

    Cleaned up definition files.

--- dornen/segments/02/music.py
+++ dornen/segments/02/music.py
@@ -163,4 +163,7 @@ maker = baca.SegmentMaker(
 accumulator.populate_segment_maker(maker)
 
 if __name__ == "__main__":
-    baca.build.make_segment_pdf(maker, runtime=baca.segments(runtime=True))
+    baca.build.make_segment_pdf(
+        maker,
+        **baca.segments(runtime=True),
+    )

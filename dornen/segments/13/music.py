d19cc00abcf7b015e833bc7197adeef13e78bbf2
Trevor Bača <trevor.baca@gmail.com>
2021-10-05 12:18:12 +0200

    Bundled LilyPond file creation keywords.

--- dornen/segments/13/music.py
+++ dornen/segments/13/music.py
@@ -601,6 +601,10 @@ if __name__ == "__main__":
         do_not_require_margin_markup=True,
         error_on_not_yet_pitched=True,
         global_rests_in_topmost_staff=True,
+        lilypond_file_keywords=baca.make_lilypond_file_dictionary(
+            include_layout_ly=True,
+            includes=["../../stylesheet.ily"],
+        ),
         score=score,
         transpose_score=True,
     )

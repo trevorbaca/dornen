643550de2533b7ea2ea549d2c1139f9259d6ea20
Trevor Bača <trevor.baca@gmail.com>
2022-01-30 16:53:48 +0100

    Bumped Python to 3.10.

--- dornen/__init__.py
+++ dornen/__init__.py
@@ -4,8 +4,8 @@ from . import library
 
 __all__ = ["library"]
 
-if sys.version_info[:2] < (3, 9):
-    raise ImportError("Requires Python 3.9 or later")
+if sys.version_info[:2] < (3, 10):
+    raise ImportError("Requires Python 3.10 or later")
 del sys
 
 """

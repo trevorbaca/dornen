8658427f4fb1157399bf1cd7003db901a53682df
Trevor Bača <trevor.baca@gmail.com>
2022-08-16 08:03:05 -0400

    Bumped LilyPond to 2.23.11.

--- dornen/sections/04/music.py
+++ dornen/sections/04/music.py
@@ -306,7 +306,7 @@ del defaults["check_wellformedness"]
 
 
 def main():
-    previous_persist = baca.previous_metadata(__file__, file_name="__persist__")
+    previous_persist = baca.previous_persist(__file__)
     baca.reapply(accumulator, accumulator.manifests(), previous_persist, voice_names)
     cache = baca.interpret.cache_leaves(
         score,

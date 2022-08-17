252b66b57591dc365173ed28b0323c137960dd2b
Trevor Bača <trevor.baca@gmail.com>
2022-08-17 09:28:31 -0400

    Cleaned up music.py files

--- dornen/sections/04/music.py
+++ dornen/sections/04/music.py
@@ -307,7 +307,12 @@ del defaults["check_wellformedness"]
 
 def main():
     previous_persist = baca.previous_persist(__file__)
-    baca.reapply(accumulator, accumulator.manifests(), previous_persist, voice_names)
+    previous_persistent_indicators = previous_persist["persistent_indicators"]
+    baca.reapply_new(
+        accumulator.voices(),
+        accumulator.manifests(),
+        previous_persistent_indicators,
+    )
     cache = baca.interpret.cache_leaves(
         score,
         len(accumulator.time_signatures),

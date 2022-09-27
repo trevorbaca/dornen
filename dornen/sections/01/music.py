321abc7e22b884a02e442ed1160139ed007c8469
Trevor Bača <trevor.baca@gmail.com>
2022-09-27 10:25:35 -0400

    Cleaned up metadata.

--- dornen/sections/01/music.py
+++ dornen/sections/01/music.py
@@ -315,8 +315,8 @@ def main():
     )
     baca.build.persist_lilypond_file(
         lilypond_file,
-        environment.metadata,
-        environment.persist,
+        metadata,
+        persist,
         timing,
         environment.arguments,
     )

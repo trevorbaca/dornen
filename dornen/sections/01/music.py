da044ea7b9aac6d543dc967ac9ca838c89b41952
Trevor Bača <trevor.baca@gmail.com>
2022-10-03 06:17:39 -0400

    Cleaned up timing.

--- dornen/sections/01/music.py
+++ dornen/sections/01/music.py
@@ -287,8 +287,7 @@ def make_score():
 
 def main():
     environment = baca.build.read_environment(__file__, baca.build.argv())
-    timing = baca.build.Timing()
-    score, measures = make_score(timing)
+    score, measures = make_score(environment.timing)
     metadata, persist = baca.section.postprocess_score(
         score,
         measures(),
@@ -301,7 +300,6 @@ def main():
         error_on_not_yet_pitched=True,
         global_rests_in_topmost_staff=True,
         manifests=library.manifests,
-        timing=timing,
         transpose_score=True,
     )
     lilypond_file = baca.lilypond.file(
@@ -313,7 +311,7 @@ def main():
         lilypond_file,
         metadata,
         persist,
-        timing,
+        environment.timing,
         environment.arguments,
     )
 

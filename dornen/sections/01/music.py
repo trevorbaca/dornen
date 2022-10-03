946796425b4d978f5eb7ef3ed62954ca88a6b4e3
Trevor Bača <trevor.baca@gmail.com>
2022-10-03 09:38:44 -0400

    Cleaned up metadata.

--- dornen/sections/01/music.py
+++ dornen/sections/01/music.py
@@ -288,7 +288,7 @@ def make_score():
 def main():
     environment = baca.build.read_environment(__file__, baca.build.argv())
     score, measures = make_score(environment.timing)
-    metadata, persist = baca.section.postprocess_score(
+    metadata = baca.section.postprocess_score(
         score,
         measures(),
         **baca.section.section_defaults(),
@@ -310,7 +310,6 @@ def main():
     baca.build.persist_lilypond_file(
         lilypond_file,
         metadata,
-        persist,
         environment.timing,
         environment.arguments,
     )

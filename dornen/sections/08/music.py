e8facf9c3b826d04a285f3a760a5f6d7b89374d2
Trevor Bača <trevor.baca@gmail.com>
2023-03-22 11:48:01 -0400

    Cleaned up music.py files.

--- dornen/sections/08/music.py
+++ dornen/sections/08/music.py
@@ -1,4 +1,6 @@
+import abjad
 import baca
+from abjadext import rmakers
 
 from dornen import library
 
@@ -77,6 +79,7 @@ def make_empty_score(first_measure_number, previous_persistent_indicators):
         assert collections == [[11, 1, 4, 8]]
         tuplets, tsd = library.make_glissando_scatto(collections)
         accumulator(library.v1, tuplets, tsd, "L3")
+    rmakers.hide_trivial(score)
     voices = baca.section.cache_voices(score, library.voice_abbreviations)
     time_signatures = baca.section.time_signatures(accumulator.time_signatures)
     baca.section.set_up_score(

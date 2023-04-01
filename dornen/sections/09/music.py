62276cc8674eafd74559c48f9c38dd1b4fa5a5e4
Trevor Bača <trevor.baca@gmail.com>
2023-04-01 13:30:57 -0400

    Cleaned up music.py files.

--- dornen/sections/09/music.py
+++ dornen/sections/09/music.py
@@ -129,7 +129,7 @@ def make_empty_score(first_measure_number, previous_persistent_indicators):
         accumulator(library.v2, tuplets, tsd, "20_1")
     rmakers.swap_trivial(score)
     voices = baca.section.cache_voices(score, library.voice_abbreviations)
-    time_signatures = baca.section.time_signatures(accumulator.time_signatures)
+    time_signatures = baca.section.wrap(accumulator.time_signatures)
     baca.section.set_up_score(
         score,
         time_signatures(),

efe5f94c596c75eb60bcd587b311f26ddbf67f68
Trevor Bača <trevor.baca@gmail.com>
2023-04-02 13:49:05 -0400

    Cleaned up music.py files.

--- dornen/sections/08/music.py
+++ dornen/sections/08/music.py
@@ -117,7 +117,7 @@ def make_score(first_measure_number, previous_persistent_indicators):
         score, first_measure_number, previous_persistent_indicators
     )
     SKIPS(score["Skips"])
-    baca.section.reapply(
+    baca.section.reapply_persistent_indicators(
         voices,
         previous_persistent_indicators,
         manifests=library.manifests,

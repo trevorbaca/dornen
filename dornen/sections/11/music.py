d3f12ae90ea3b092a71d8762714a9718fcc952c6
Trevor Bača <trevor.baca@gmail.com>
2024-02-11 18:22:39 -0500

    Cleaned up music.py files.

--- dornen/sections/11/music.py
+++ dornen/sections/11/music.py
@@ -98,31 +98,31 @@ def postprocess(cache):
     @baca.call(baca.select.rleaves(m[12]))
     def _(o):
         baca.spanners.hairpin(o, "pp < p")
-        baca.mspanners.text(o, "trans. => kn. rasg.")
+        baca.mspanners.text([o], "trans. => kn. rasg.")
 
     @baca.call(baca.select.rleaves(m[14]))
     def _(o):
         baca.spanners.hairpin(o, "p > pp")
-        baca.mspanners.text(o, "trans. => tamb. tr.")
+        baca.mspanners.text([o], "trans. => tamb. tr.")
 
     @baca.call(baca.select.rleaves(m[16]))
     def _(o):
         baca.spanners.hairpin(o, "pp < mp")
-        baca.mspanners.text(o, "trans. => kn. rasg.")
+        baca.mspanners.text([o], "trans. => kn. rasg.")
 
     @baca.call(m[18])
     def _(o):
         baca.spanners.hairpin(baca.select.rleaves(o), "mp > pp")
-        baca.mspanners.text(baca.select.rleaves(o), "trans. => tamb. tr.")
+        baca.mspanners.text([baca.select.rleaves(o)], "trans. => tamb. tr.")
 
     @baca.call(m[20])
     def _(o):
-        baca.mspanners.text(baca.select.rleaves(o), "trans. => kn. rasg.")
+        baca.mspanners.text([baca.select.rleaves(o)], "trans. => kn. rasg.")
 
     @baca.call(baca.select.rleaves(m.get(22, 24)))
     def _(o):
         baca.spanners.hairpin(o, "mf > p")
-        baca.mspanners.text(o, "trans. => tamb. tr.")
+        baca.mspanners.text([o], "trans. => tamb. tr.")
 
     @baca.call(m.get(1, 11))
     def _(o):

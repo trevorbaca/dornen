8bf6d8b3e064c3465eb855352ff3d8e0ddb1daf2
Trevor Bača <trevor.baca@gmail.com>
2024-06-07 17:05:10 -0400

    Set lilypond_timeout=30.

--- dornen/sections/03/music.py
+++ dornen/sections/03/music.py
@@ -439,6 +439,7 @@ def persist_score(score, environment):
         environment.timing,
         lilypond_file,
         metadata,
+        lilypond_timeout=30,
     )
 
 
--- dornen/sections/06/music.py
+++ dornen/sections/06/music.py
@@ -199,6 +199,7 @@ def persist_score(score, environment):
         environment.timing,
         lilypond_file,
         metadata,
+        lilypond_timeout=30,
     )
 
 

f5a66572a67f3d7e52ca67d3e8edae30b740f090
Trevor Bača <trevor.baca@gmail.com>
2024-06-17 14:42:15 -0400

    Cleaned up layout files.

--- dornen/sections/06/music.py
+++ dornen/sections/06/music.py
@@ -203,7 +203,7 @@ def persist_score(score, environment):
     )
 
 
-def make_layout():
+def make_layout(environment):
     breaks = baca.layout.Breaks(
         baca.layout.Page(
             1,
@@ -216,7 +216,12 @@ def make_layout():
     spacing = baca.layout.Spacing(
         default=(1, 180),
     )
-    baca.build.write_layout_ily(breaks, spacing)
+    baca.build.write_layout_ily(
+        breaks,
+        environment.metadata["time_signatures"],
+        spacing,
+        first_measure_number=environment.first_measure_number,
+    )
 
 
 def main():
@@ -229,7 +234,7 @@ def main():
         )
         persist_score(score, environment)
     if environment.arguments.layout:
-        make_layout()
+        make_layout(environment)
 
 
 if __name__ == "__main__":

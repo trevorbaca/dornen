88170afb26f21b655874cd3e45588a30053cf380
Trevor Bača <trevor.baca@gmail.com>
2025-01-07 15:43:49 -0500

    Clean up __init__.py

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
         name="dornen",
         packages=["dornen"],
         platforms="Any",
-        python_requires=">=3.10",
+        python_requires=">=3.12",
         url="https://github.com/trevorbaca/dornen",
         version="3.19",
     )
